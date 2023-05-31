62758354e6a35515a18c4037a5ef5960148c066a
Cosmic-rare <ysolakey1116@gmail.com>
2023-06-01 06:03:46 +0900

    やることを明確に

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import data1 from './data1.json';
 import axios from "axios"
 import Tournament from '@/components/Tournament';
+import draw from '@/util/draw';
 
 export interface TournamentCellData {
   text?: string;
@@ -13,7 +14,7 @@ export interface TournamentCellData {
   point?: string;
 }
 
-interface MyObject {
+export interface MyObject {
   [key: string]: any;
 }
 
@@ -28,41 +29,31 @@ interface Score {
 const Main: React.FC<{ data: Record<string, TournamentCellData> }> = ({ data }) => {
   const [d, setD] = useState<Score[]>([])
   const [d2, setD2] = useState(data)
+
   const getData = async () => {
     const da = await axios.get("http://localhost:3000/api/hello")
     setD(da.data.data)
   }
+
   useEffect(() => {
     try {
       getData()
     } catch (err) {}
   }, [])
+  
   useEffect(() => {
     let d3:MyObject = {}
     d.forEach((val) => {
       d3[val.position.toString()] = val
     })
 
-    if (Object.keys(d3).length !== 0) {
-      if (d3["1"].score > d3["2"].score) {
-        setD2((d2_p) => {d2_p["1_1"] = {border_left: 2, border_top: 2};return d2_p})
-      } else if (d3["2"].score > d3["1"].score) {
-        setD2((d2_p) => {d2_p["2_1"] = {border_top: 2};d2_p["3_1"] = {border_left: 2};return d2_p})
-      } else {
-      }
-    }
+    draw(d3, setD2)
   }, [d, d2])
 
   return (
     <>
       <div style={{ width: `${30 * 15}px`, height: `320px`, overflowX: 'hidden', position: "relative" }}>
         <Tournament cells={d2} />
-      </div>
-      <div>
-        <code>
-          {JSON.stringify(d2)}
-          {JSON.stringify(d)}
-        </code>
       </div>
     </>
   );
