fc09bb7c9f5ecf4219cfe4fd1bc6ec95f45cc4ab
Cosmic-rare <ysolakey1116@gmail.com>
2024-06-19 17:40:09 +0900

    S3にデプロイできるように

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: process.env.DEPLOY == "s3" ? "https://s3.tani-exe.net/tournament2024" : null,
+  basePath: process.env.DEPLOY == "s3" ? "/tournament2024" : null,
   reactStrictMode: true,
   webpack: (config) => {
     config.module.rules.push({
