943a7819eb2e127663f008ecd6eb1b9f24276ae4
Cosmic-rare <ysolakey1116@gmail.com>
2024-07-01 18:14:36 +0900

    軽く点数表示

--- components/top/pointModal.tsx
+++ components/top/pointModal.tsx
@@ -1,10 +1,40 @@
 import { Modal, Radio, Space, Button } from "antd"
+import { Grid } from "@mui/material"
 
 interface Props {
   isModalOpen: boolean
   setIsModalOpen: Function
   data: any
   event: string
+}
+
+const Point = ({ l, h, t }: any) => {
+  return (
+    <Grid container spacing={2} style={{ justifyContent: "center", textAlign: "center", fontSize: "1.1em" }}>
+      <Grid item xs={2}>
+        <div>{t}</div>
+      </Grid>
+      <Grid item xs={4}>
+        <div>{l}</div>
+      </Grid>
+      <Grid item xs={2}>
+        <div>-</div>
+      </Grid>
+      <Grid item xs={4}>
+        <div>{h}</div>
+      </Grid>
+    </Grid>
+  )
+}
+
+const PK = ({ pk }: any) => {
+  return (
+    <Grid container spacing={2} style={{ justifyContent: "center", textAlign: "center", fontSize: "1.1em" }}>
+      <Grid item xs={12}>
+        <div>{JSON.stringify(pk)}</div>
+      </Grid>
+    </Grid>
+  )
 }
 
 const PointModal: React.FC<Props> = ({ isModalOpen, setIsModalOpen, data, event }) => {
@@ -18,9 +48,13 @@ const PointModal: React.FC<Props> = ({ isModalOpen, setIsModalOpen, data, event 
       footer={[]}
     >
       <div style={{ position: "relative" }}>
-        <code>{JSON.stringify(data)}</code>
-        <code>{event}</code>
         {/* eventで分けながらdata表示 */}
+        <h4>得点</h4>
+        <Point l={data?.l_p1} h={data?.h_p1} t="1st" />
+        <Point l={data?.l_p2} h={data?.h_p2} t="2nd" />
+        <Point l={data?.l_p3} h={data?.h_p3} t="3rd" />
+
+        {event == "soccer" ? <PK pk={data?.pk} /> : null}
       </div>
     </Modal>
   )
