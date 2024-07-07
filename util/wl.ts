5698b956e0b34a71fad254340694617ea1eb3310
Cosmic-rare <ysolakey1116@gmail.com>
2024-07-07 21:30:16 +0900

    デバッグ

--- util/wl.ts
+++ util/wl.ts
@@ -5,13 +5,13 @@ export const wl = (d: gameType, e: string) => {
     // p1~p3で判別 (1,2がnullなら未決着カウント)
     let r = 0 // +l -h
     if (!(d.l_p1 == null || d.h_p1 == null)) {
-      r = r + d.l_p1 > d.h_p1 ? 1 : -1
+      r = r + (d.l_p1 > d.h_p1 ? 1 : -1)
     } else return null
     if (!(d.l_p2 == null || d.h_p2 == null)) {
-      r = r + d.l_p2 > d.h_p2 ? 1 : -1
+      r = r + (d.l_p2 > d.h_p2 ? 1 : -1)
     } else return null
     if (!(d.l_p3 == null || d.h_p3 == null)) {
-      r = r + d.l_p3 > d.h_p3 ? 1 : -1
+      r = r + (d.l_p3 > d.h_p3 ? 1 : -1)
     }
     return r > 0 ? "l" : "h"
   } else if ("dodgeball" == e) {
