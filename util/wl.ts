198db3fb80fe126a324f144d74e1906050f5aa9d
Cosmic-rare <ysolakey1116@gmail.com>
2024-07-11 23:33:48 +0900

    ドッジを反転

--- util/wl.ts
+++ util/wl.ts
@@ -19,19 +19,19 @@ export const wl = (d: gameType, e: string, a: boolean, da: boolean) => {
     // p1~p3で判別 (それぞれにfHitted)
     let r = 0 // +l -h
     if (d.l_p1 == d.h_p1 && d.l_p1 != null) {
-      r = r + (d.fHitted.p1 == "l" ? 1 : -1)
+      r = r + (d.fHitted.p1 == "l" ? -1 : 1)
     } else if (!(d.l_p1 == null || d.h_p1 == null)) {
-      r = r + (d.l_p1 > d.h_p1 ? 1 : -1)
+      r = r + (d.l_p1 > d.h_p1 ? -1 : 1)
     } else return null
     if (d.l_p2 == d.h_p2 && d.l_p2 != null) {
-      r = r + (d.fHitted.p2 == "l" ? 1 : -1)
+      r = r + (d.fHitted.p2 == "l" ? -1 : 1)
     } else if (!(d.l_p2 == null || d.h_p2 == null)) {
-      r = r + (d.l_p2 > d.h_p2 ? 1 : -1)
+      r = r + (d.l_p2 > d.h_p2 ? -1 : 1)
     } else return null
     if (d.l_p3 == d.h_p3 && d.l_p3 != null) {
-      r = r + (d.fHitted.p3 == "l" ? 1 : -1)
+      r = r + (d.fHitted.p3 == "l" ? -1 : 1)
     } else if (!(d.l_p3 == null || d.h_p3 == null)) {
-      r = r + (d.l_p3 > d.h_p3 ? 1 : -1)
+      r = r + (d.l_p3 > d.h_p3 ? -1 : 1)
     }
     return r > 0 ? "l" : "h"
   } else if ("esport" == e) {
